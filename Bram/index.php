5a40fb61c06bba7bebbd6c3f7fd10a51c7dcd815
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-04-07 10:12:58 +0200

    beuk

---
+++ Bram/index.php
@@ -0,0 +1,22 @@
+<?php ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>De Elstar</title>
+    
+        <link rel="icon" type="image/png" href="../Assets//Img/2020_Packshot_Elstar_500x500px-300x300.png">
+
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <span class="align-baseline">baseline</span>
+        <span class="align-top">top</span>
+        <span class="align-middle">middle</span>
+        <span class="align-bottom">bottom</span>
+        <span class="align-text-top">text-top</span>
+        <span class="align-text-bottom">text-bottom</span>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,6 @@ echo "Hello World";
         <meta charset="UTF-8">
 		<title>De Elstar</title>
 
-        <link rel="icon" type="image/png" href="Assets/Img/2020_Packshot_Elstar_500x500px-300x300.png">
+        <link rel="icon" type="image/png" href="./Assets/Img/2020_Packshot_Elstar_500x500px-300x300.png">
     </head>
 </html>
