81ca054efeaf7856f6abc774356b8199aeb46ed5
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-04-07 08:50:39 +0200

    funny

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello World";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<title>De Elstar</title>
+
+        <link rel="icon" type="image/png" href="Assets/Img/2020_Packshot_Elstar_500x500px-300x300.png">
+    </head>
+</html>
