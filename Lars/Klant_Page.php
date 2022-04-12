b874447371ba86b3d9c95e44d2e93ee8d68f645f
Lars_Twi <tijssenlars@gmail.com>
2022-04-12 10:51:31 +0200

    Klanten_PageV5
    Functional Form

--- Lars/Klant_Page.php
+++ Lars/Klant_Page.php
@@ -11,16 +11,27 @@
     <link href="newcss.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container flex">
-        <div class="flex">
-            <div class="input-container">
-                <form action="" method="POST">
-                <h1>Vul hier uw Gegevens In</h1> 
-                <input type="text" name="v_naam" class="question" required autocomplete="off" />
-                <label for="v_naam"><span>Wat is uw Naam?</span></label>  
-                <input type="submit" value="Submit!">
-                </form>       
-            </div>
+    <div class="container">
+        <div class="input-container">
+            <form action="" method="POST">
+            <h1>Vul hier uw Gegevens In</h1> 
+            <input type="text" name="v_naam" class="question" required autocomplete="off" />
+            <label for="v_naam"><span>Wat is uw Naam?</span></label>
+
+            <input type="text" name="a_naam" class="question" required autocomplete="off" />
+            <label for="a_naam"><span>Wat is uw Achternaam?</span></label> 
+
+            <input type="text" name="email" class="question" required autocomplete="off" />
+            <label for="email"><span>Wat is uw Email?</span></label> 
+
+            <input type="text" name="postcode" class="question" required autocomplete="off" />
+            <label for="postcode"><span>Wat is uw Postcode?</span></label> 
+
+            <input type="text" name="huisnummer" class="question" required autocomplete="off" />
+            <label for="huisnummer"><span>Wat is uw HuisNummer?</span></label> 
+
+            <input type="submit" value="Submit!">
+            </form>       
         </div>
     </div>
 </body>
